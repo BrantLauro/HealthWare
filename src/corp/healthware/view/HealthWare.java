6d420a7b6f95e633857163df6f2a95e51002e75c
Renan Iago <renanfciriaco9@gmail.com>
2023-11-23 23:36:12 -0300

    eae

--- src/corp/healthware/view/HealthWare.java
+++ src/corp/healthware/view/HealthWare.java
@@ -5,7 +5,6 @@ import com.formdev.flatlaf.themes.FlatMacLightLaf;
 import javax.swing.UIManager;
 
 public class HealthWare {
-    // tana vai me mamar
     public static void main(String[] args) {
         try {
             UIManager.setLookAndFeel( new FlatMacLightLaf() );
