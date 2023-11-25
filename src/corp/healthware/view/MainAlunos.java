d688fff63e581a39c515fb199425976751d4e759
BrantLauro <brantlauro@gmail.com>
2023-11-25 03:13:52 -0300

    :lipstick: CentralFrame retirado de MainAlunos

--- src/corp/healthware/view/MainAlunos.java
+++ src/corp/healthware/view/MainAlunos.java
@@ -1,11 +1,9 @@
 package corp.healthware.view;
 
-import com.formdev.flatlaf.FlatClientProperties;
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Insets;
 import javax.swing.UIManager;
-import javax.swing.table.DefaultTableModel;
+
 
 public class MainAlunos extends javax.swing.JPanel {
 
@@ -13,13 +11,14 @@ public class MainAlunos extends javax.swing.JPanel {
         UIManager.put("ScrollBar.thumbArc", 10);
         UIManager.put("ScrollBar.thumbInsets", new Insets(2, 2, 2, 2));
         initComponents();
-        jTextFieldPesquisa.putClientProperty(FlatClientProperties.STYLE, "arc: 9");
-        DefaultTableModel tableModel = (DefaultTableModel) jTableAlunos.getModel();
-        tableModel.setRowCount(0);
+        AlunosCentralFrame central = new AlunosCentralFrame();
+        central.setSize(820, 570);
+        central.setLocation(0, 0);
+        jPanelCentral.removeAll();
+        jPanelCentral.add(central, BorderLayout.CENTER);
+        jPanelCentral.revalidate();
+        jPanelCentral.repaint();
         
-        tableModel.addRow(new Object[]{"Lucas Colen", "Pilates", "Devedor", "Dia 19", ""});
-        tableModel.addRow(new Object[]{"Lucas Costa", "Yoga", "Devedor", "Dia 19", ""});
-        jTableAlunos.setModel(tableModel);
     }
 
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -32,11 +31,6 @@ public class MainAlunos extends javax.swing.JPanel {
         jButtonNovaMod = new javax.swing.JButton();
         jLabelLogo = new javax.swing.JLabel();
         jPanelCentral = new javax.swing.JPanel();
-        jScrollPaneTabela = new javax.swing.JScrollPane();
-        jTableAlunos = new javax.swing.JTable();
-        jTextFieldPesquisa = new javax.swing.JTextField();
-        jPanelIcone = new javax.swing.JPanel();
-        jLabelPesquisa = new javax.swing.JLabel();
 
         setPreferredSize(new java.awt.Dimension(100, 570));
 
@@ -110,120 +104,15 @@ public class MainAlunos extends javax.swing.JPanel {
 
         jPanelCentral.setBackground(new java.awt.Color(239, 239, 239));
 
-        jScrollPaneTabela.setBackground(new java.awt.Color(223, 223, 223));
-        jScrollPaneTabela.setToolTipText("");
-
-        jTableAlunos.setBackground(new java.awt.Color(223, 223, 223));
-        jTableAlunos.setFont(new java.awt.Font("TT Hoves Pro Trial", 0, 12)); // NOI18N
-        jTableAlunos.setForeground(new java.awt.Color(41, 41, 41));
-        jTableAlunos.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null}
-            },
-            new String [] {
-                "Nome", "Modalidade", "Status", "Vencimento", "Ação"
-            }
-        ));
-        jTableAlunos.setEnabled(false);
-        jTableAlunos.setFocusable(false);
-        jTableAlunos.setGridColor(new java.awt.Color(239, 239, 239));
-        jTableAlunos.setIntercellSpacing(new java.awt.Dimension(10, 0));
-        jTableAlunos.setRequestFocusEnabled(false);
-        jTableAlunos.setRowHeight(40);
-        jTableAlunos.setSelectionBackground(new java.awt.Color(239, 239, 239));
-        jTableAlunos.setSelectionForeground(new java.awt.Color(41, 41, 41));
-        jTableAlunos.setShowGrid(false);
-        jTableAlunos.setShowVerticalLines(true);
-        jTableAlunos.getTableHeader().setResizingAllowed(false);
-        jTableAlunos.getTableHeader().setReorderingAllowed(false);
-        jScrollPaneTabela.setViewportView(jTableAlunos);
-
-        jTextFieldPesquisa.setBackground(new java.awt.Color(223, 223, 223));
-        jTextFieldPesquisa.setFont(new java.awt.Font("Rosario", 1, 14)); // NOI18N
-        jTextFieldPesquisa.setForeground(new java.awt.Color(41, 41, 41));
-        jTextFieldPesquisa.setText("Pesquisar");
-        jTextFieldPesquisa.setToolTipText("");
-        jTextFieldPesquisa.addFocusListener(new java.awt.event.FocusAdapter() {
-            public void focusGained(java.awt.event.FocusEvent evt) {
-                jTextFieldPesquisaFocusGained(evt);
-            }
-        });
-        jTextFieldPesquisa.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextFieldPesquisaActionPerformed(evt);
-            }
-        });
-
-        jPanelIcone.setBackground(new java.awt.Color(239, 239, 239));
-
-        jLabelPesquisa.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/pesquisar.png"))); // NOI18N
-
-        javax.swing.GroupLayout jPanelIconeLayout = new javax.swing.GroupLayout(jPanelIcone);
-        jPanelIcone.setLayout(jPanelIconeLayout);
-        jPanelIconeLayout.setHorizontalGroup(
-            jPanelIconeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 42, Short.MAX_VALUE)
-            .addGroup(jPanelIconeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelIconeLayout.createSequentialGroup()
-                    .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jLabelPesquisa)
-                    .addContainerGap()))
-        );
-        jPanelIconeLayout.setVerticalGroup(
-            jPanelIconeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 42, Short.MAX_VALUE)
-            .addGroup(jPanelIconeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelIconeLayout.createSequentialGroup()
-                    .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jLabelPesquisa)
-                    .addContainerGap()))
-        );
-
         javax.swing.GroupLayout jPanelCentralLayout = new javax.swing.GroupLayout(jPanelCentral);
         jPanelCentral.setLayout(jPanelCentralLayout);
         jPanelCentralLayout.setHorizontalGroup(
             jPanelCentralLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanelCentralLayout.createSequentialGroup()
-                .addGroup(jPanelCentralLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanelCentralLayout.createSequentialGroup()
-                        .addGap(46, 46, 46)
-                        .addComponent(jTextFieldPesquisa, javax.swing.GroupLayout.PREFERRED_SIZE, 656, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jPanelIcone, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanelCentralLayout.createSequentialGroup()
-                        .addGap(17, 17, 17)
-                        .addComponent(jScrollPaneTabela, javax.swing.GroupLayout.PREFERRED_SIZE, 764, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(38, Short.MAX_VALUE))
+            .addGap(0, 787, Short.MAX_VALUE)
         );
         jPanelCentralLayout.setVerticalGroup(
             jPanelCentralLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanelCentralLayout.createSequentialGroup()
-                .addGap(52, 52, 52)
-                .addGroup(jPanelCentralLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jTextFieldPesquisa, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jPanelIcone, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addComponent(jScrollPaneTabela, javax.swing.GroupLayout.PREFERRED_SIZE, 433, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(25, Short.MAX_VALUE))
+            .addGap(0, 570, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout jPanelContentLayout = new javax.swing.GroupLayout(jPanelContent);
@@ -245,7 +134,7 @@ public class MainAlunos extends javax.swing.JPanel {
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanelContent, javax.swing.GroupLayout.DEFAULT_SIZE, 1012, Short.MAX_VALUE)
+            .addComponent(jPanelContent, javax.swing.GroupLayout.DEFAULT_SIZE, 980, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -274,16 +163,14 @@ public class MainAlunos extends javax.swing.JPanel {
     }//GEN-LAST:event_jButtonNovoAlunoActionPerformed
 
     private void jButtonNovaModActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonNovaModActionPerformed
-        // TODO add your handling code here:
+        NovaModFrame tCMod = new NovaModFrame();
+        tCMod.setSize(820, 570);
+        tCMod.setLocation(0, 0);
+        jPanelCentral.removeAll();
+        jPanelCentral.add(tCMod, BorderLayout.CENTER);
+        jPanelCentral.revalidate();
+        jPanelCentral.repaint();
     }//GEN-LAST:event_jButtonNovaModActionPerformed
-
-    private void jTextFieldPesquisaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextFieldPesquisaActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jTextFieldPesquisaActionPerformed
-
-    private void jTextFieldPesquisaFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jTextFieldPesquisaFocusGained
-        jTextFieldPesquisa.setText("");
-    }//GEN-LAST:event_jTextFieldPesquisaFocusGained
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -291,13 +178,8 @@ public class MainAlunos extends javax.swing.JPanel {
     private javax.swing.JButton jButtonNovoAluno;
     private javax.swing.JButton jButtonVoltar;
     private javax.swing.JLabel jLabelLogo;
-    private javax.swing.JLabel jLabelPesquisa;
     private javax.swing.JPanel jPanelCentral;
     private javax.swing.JPanel jPanelContent;
-    private javax.swing.JPanel jPanelIcone;
     private javax.swing.JPanel jPanelLateral;
-    private javax.swing.JScrollPane jScrollPaneTabela;
-    private javax.swing.JTable jTableAlunos;
-    private javax.swing.JTextField jTextFieldPesquisa;
     // End of variables declaration//GEN-END:variables
 }
